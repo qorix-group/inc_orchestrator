2f848e7048d232fe8506db89824ef957b5526ea6
Pawel Rutka <pawel.rutka.ext@qorix.ai>
2025-05-28 13:51:06 +0200

    orch: split err handling
    Split execute errors from real task flow errors
    
    Signed-off-by: Pawel Rutka <pawel.rutka.ext@qorix.ai>

--- src/orchestration/src/actions/internal/action.rs
+++ src/orchestration/src/actions/internal/action.rs
@@ -16,12 +16,54 @@ use crate::common::tag::Tag;
 use async_runtime::futures::reusable_box_future::ReusableBoxFuture;
 use foundation::prelude::CommonErrors;
 
-use std::fmt::{Debug, Formatter};
+use std::{
+    fmt::{Debug, Formatter},
+    ops::Deref,
+};
+
+/// Represents a user-defined error value that can be propagated through the action execution chain.
+/// This allows user code to signal specific errors that can be handled or logged by the orchestrator.
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct UserErrValue(u64);
+
+impl Deref for UserErrValue {
+    type Target = u64;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl From<u64> for UserErrValue {
+    fn from(value: u64) -> Self {
+        UserErrValue(value)
+    }
+}
+
+#[allow(clippy::from_over_into)]
+impl Into<ActionExecError> for UserErrValue {
+    fn into(self) -> ActionExecError {
+        ActionExecError::UserError(self)
+    }
+}
+
+/// Enum representing possible errors that can occur during action execution.
+///
+/// Variants:
+/// - `UserError(UserErrValue)`: Indicates an error returned by user code, allowing it to propagate through the chain. It means signature to `Invoke` needs to capture Futures/functions with Result<(), UserErrValue>
+/// - `NonRecoverableFailure`: Represents a failure that cannot be recovered from.
+/// - `Internal`: Placeholder for internal errors, with potential for expansion as needed.
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum ActionExecError {
+    UserError(UserErrValue),
+    NonRecoverableFailure,
+    Internal, // TODO add more errors if needed
+}
 
 ///
-/// Result to indicate the given action status. [`Ok(())`] if everything went fine, Err(CommonErrors) to mark error in execution.
+/// Result to indicate the given action status. [`Ok(())`] if everything went fine, Err(ActionExecError) to mark error in execution.
 ///
-pub type ActionResult = Result<(), CommonErrors>;
+pub type ActionResult = Result<(), ActionExecError>;
 
 ///
 /// Result to indicate the acquisition status of the reusable (boxed) future. [`Ok(ReusableBoxFuture<ActionResult>)`] if everything went fine, Err(CommonErrors) to mark error in execution.
