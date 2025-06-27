14d4c49206167a1e6bbb46cf29cc11942af67844
Prabakaran Chinnasamy <Prabakaran.Chinnasamy@qorix.ai>
2025-06-27 07:52:43 +0530

    orch: Port component integration tests
    Ported component integration tests based on new APIs.

--- component_integration_tests/rust_test_scenarios/src/tests/orchestration/mod.rs
+++ component_integration_tests/rust_test_scenarios/src/tests/orchestration/mod.rs
@@ -1,7 +1,7 @@
+use async_runtime::futures::{reusable_box_future::ReusableBoxFuturePool, yield_now};
+use orchestration::common::tag::Tag;
+use orchestration::prelude::*;
 use std::pin::Pin;
-
-use async_runtime::{core::types::box_future, futures::yield_now};
-use orchestration::prelude::{ActionResult, ActionTrait};
 use tracing::info;
 
 pub mod orchestration_concurrency;
@@ -9,33 +9,42 @@ pub mod orchestration_sequence;
 pub mod orchestration_test_group;
 
 pub struct JustLogAction {
+    base: ActionBaseMeta,
     name: String,
 }
 
 impl JustLogAction {
     fn new(name: impl Into<String>) -> Box<JustLogAction> {
-        Box::new(Self { name: name.into() })
+        const DEFAULT_TAG: &str = "integration::tests::just_log_action";
+
+        Box::new(Self {
+            base: ActionBaseMeta {
+                tag: Tag::from_str_static(DEFAULT_TAG),
+                reusable_future_pool: ReusableBoxFuturePool::new(1, Self::execute_impl("JustLogAction".into())),
+            },
+            name: name.into(),
+        })
+    }
+    async fn execute_impl(name: String) -> ActionResult {
+        info!("{name} was executed");
+        Ok(())
     }
 }
 
 impl ActionTrait for JustLogAction {
-    fn execute(&mut self) -> orchestration::actions::action::ActionFuture {
-        let name = self.name.clone();
-        box_future(async move {
-            info!("{name} was executed");
-            Ok(())
-        })
-    }
     fn name(&self) -> &'static str {
         "JustLogAction"
     }
     fn dbg_fmt(&self, _nest: usize, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         Ok(())
     }
-    fn fill_runtime_info(&mut self, _p: &mut orchestration::actions::action::ActionRuntimeInfoProvider) {}
+    fn try_execute(&mut self) -> ReusableBoxFutureResult {
+        self.base.reusable_future_pool.next(JustLogAction::execute_impl(self.name.clone()))
+    }
 }
 
 /// emulate some sleep as workaround until sleep is supported in runtime
+#[allow(dead_code)]
 fn busy_sleep() -> ActionResult {
     info!("Start sleeping");
     let mut ctr = 1_000_000;
@@ -46,6 +55,7 @@ fn busy_sleep() -> ActionResult {
     Ok(())
 }
 
+#[allow(dead_code)]
 async fn generic_test_func(name: &'static str) -> ActionResult {
     info!("Start of '{}' function", name);
     info!("'{}' function yielding...", name);
@@ -56,6 +66,45 @@ async fn generic_test_func(name: &'static str) -> ActionResult {
     rv
 }
 
+#[allow(dead_code)]
 fn factory_test_func(name: &'static str) -> impl Fn() -> Pin<Box<dyn Future<Output = ActionResult> + Send>> + Clone {
     move || Box::pin(generic_test_func(name))
+}
+
+fn generic_test_sync_func(name: &'static str) -> InvokeResult {
+    info!("Start of '{}' function", name);
+
+    info!("End of '{}' function", name);
+    Ok(())
+}
+
+pub fn function1() -> InvokeResult {
+    generic_test_sync_func("Function1")
+}
+pub fn function2() -> InvokeResult {
+    generic_test_sync_func("Function2")
+}
+pub fn function3() -> InvokeResult {
+    generic_test_sync_func("Function3")
+}
+pub fn function4() -> InvokeResult {
+    generic_test_sync_func("Function4")
+}
+pub fn function5() -> InvokeResult {
+    generic_test_sync_func("Function5")
+}
+pub fn function6() -> InvokeResult {
+    generic_test_sync_func("Function6")
+}
+pub fn outer_function1() -> InvokeResult {
+    generic_test_sync_func("OuterFunction1")
+}
+pub fn outer_function2() -> InvokeResult {
+    generic_test_sync_func("OuterFunction2")
+}
+pub fn inner_function1() -> InvokeResult {
+    generic_test_sync_func("InnerFunction1")
+}
+pub fn inner_function2() -> InvokeResult {
+    generic_test_sync_func("InnerFunction2")
 }
