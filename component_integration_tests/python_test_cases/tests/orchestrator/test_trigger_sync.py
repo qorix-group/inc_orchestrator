60c25a408c13381589c668c6c6bedab2780eca27
Igor Ostrowski <igor.ostrowski.ext@qorix.ai>
2025-07-08 09:40:44 +0200

    testing: Add tests for orchestration trigger and sync
    Includes intra and inter orchestration program synchronization

---
+++ component_integration_tests/python_test_cases/tests/orchestrator/test_trigger_sync.py
@@ -0,0 +1,138 @@
+from datetime import timedelta
+
+import pytest
+from testing_tools.log_container import LogContainer
+
+BLOCKING_TASK_ID = "blocking_sleep_task"
+BLOCKING_TASK_DELAY_MS = 1000
+BASIC_TASK_A_ID = "basic_task_A"
+BASIC_TASK_B_ID = "basic_task_B"
+
+
+class TestOneTriggerOneSyncTwoPrograms:
+    @pytest.fixture(scope="class")
+    def scenario_name(self):
+        return "orchestration.1_trigger_1_sync_2_programs"
+
+    @pytest.fixture(scope="class")
+    def test_config(self):
+        return {"runtime": {"task_queue_size": 256, "workers": 4}}
+
+    def test_execution_order(self, test_results: LogContainer):
+        results = test_results.get_logs()
+        assert len(results) == 3, "Expected 3 messages in total"
+
+        sleep_begin_msg = results.pop(0)
+        assert (
+            sleep_begin_msg.id == BLOCKING_TASK_ID
+            and sleep_begin_msg.location == "begin"
+        ), "Expected the first message to be the start of the blocking sleep task"
+
+        sleep_end_msg = results.pop(0)
+        assert (
+            sleep_end_msg.id == BLOCKING_TASK_ID and sleep_end_msg.location == "end"
+        ), "Expected the second message to be the end of the blocking sleep task"
+
+        basic_task_msg = results.pop(0)
+        assert basic_task_msg.id == BASIC_TASK_A_ID, (
+            f"Expected the third message to be {BASIC_TASK_A_ID}"
+        )
+
+    def test_execution_delay(self, test_results: LogContainer):
+        sleep_begin_msg = test_results.get_logs_by_field(
+            "id", BLOCKING_TASK_ID
+        ).find_log("location", "begin")
+        sleep_end_msg = test_results.get_logs_by_field("id", BLOCKING_TASK_ID).find_log(
+            "location", "end"
+        )
+
+        assert sleep_end_msg.timestamp - sleep_begin_msg.timestamp >= timedelta(
+            milliseconds=BLOCKING_TASK_DELAY_MS
+        ), (
+            "Expected the blocking sleep task to take at least "
+            f"{BLOCKING_TASK_DELAY_MS} ms, but it took "
+            f"{sleep_end_msg.timestamp - sleep_begin_msg.timestamp}"
+        )
+
+
+class TestOneTriggerTwoSyncsThreePrograms:
+    @pytest.fixture(scope="class")
+    def scenario_name(self):
+        return "orchestration.1_trigger_2_syncs_3_programs"
+
+    @pytest.fixture(scope="class")
+    def test_config(self):
+        return {"runtime": {"task_queue_size": 256, "workers": 4}}
+
+    def test_execution_order(self, test_results: LogContainer):
+        results = test_results.get_logs()
+        assert len(results) == 4, "Expected 4 messages in total"
+
+        sleep_begin_msg = results.pop(0)
+        assert (
+            sleep_begin_msg.id == BLOCKING_TASK_ID
+            and sleep_begin_msg.location == "begin"
+        ), "Expected the first message to be the start of the blocking sleep task"
+
+        sleep_end_msg = results.pop(0)
+        assert (
+            sleep_end_msg.id == BLOCKING_TASK_ID and sleep_end_msg.location == "end"
+        ), "Expected the second message to be the end of the blocking sleep task"
+
+        expected_basic_task_ids = {BASIC_TASK_A_ID, BASIC_TASK_B_ID}
+        basic_task_msg = results.pop(0)
+        assert basic_task_msg.id in expected_basic_task_ids, (
+            f"Expected the third message to be one of {expected_basic_task_ids}, "
+        )
+        expected_basic_task_ids.remove(basic_task_msg.id)
+
+        basic_task_msg = results.pop(0)
+        assert basic_task_msg.id in expected_basic_task_ids, (
+            "Expected the fourth message to be {expected_basic_task_ids}"
+        )
+
+    def test_execution_delay(self, test_results: LogContainer):
+        sleep_begin_msg = test_results.get_logs_by_field(
+            "id", BLOCKING_TASK_ID
+        ).find_log("location", "begin")
+        sleep_end_msg = test_results.get_logs_by_field("id", BLOCKING_TASK_ID).find_log(
+            "location", "end"
+        )
+
+        assert sleep_end_msg.timestamp - sleep_begin_msg.timestamp >= timedelta(
+            milliseconds=BLOCKING_TASK_DELAY_MS
+        ), (
+            "Expected the blocking sleep task to take at least "
+            f"{BLOCKING_TASK_DELAY_MS} ms, but it took "
+            f"{sleep_end_msg.timestamp - sleep_begin_msg.timestamp}"
+        )
+
+
+class TestTriggerAndSyncInNestedBranches(TestOneTriggerOneSyncTwoPrograms):
+    @pytest.fixture(scope="class")
+    def scenario_name(self):
+        return "orchestration.trigger_and_sync_in_nested_branches"
+
+
+class TestTriggerSyncOneAfterAnother:
+    @pytest.fixture(scope="class")
+    def scenario_name(self):
+        return "orchestration.trigger_sync_one_after_another"
+
+    @pytest.fixture(scope="class")
+    def test_config(self):
+        return {"runtime": {"task_queue_size": 256, "workers": 1}}
+
+    def test_execution_order(self, test_results: LogContainer):
+        results = test_results.get_logs()
+        assert len(results) == 2, "Expected 2 messages in total"
+
+        basic_task_a_msg = results.pop(0)
+        assert basic_task_a_msg.id == BASIC_TASK_A_ID, (
+            f"Expected the first message to be {BASIC_TASK_A_ID}"
+        )
+
+        basic_task_b_msg = results.pop(0)
+        assert basic_task_b_msg.id == BASIC_TASK_B_ID, (
+            f"Expected the first message to be {BASIC_TASK_B_ID}"
+        )
---
+++ component_integration_tests/rust_test_scenarios/src/tests/orchestration/orchestration_trigger_sync.rs
@@ -0,0 +1,319 @@
+use crate::internals::helpers::runtime_helper::Runtime;
+use crate::internals::scenario::Scenario;
+
+use super::*;
+use foundation::prelude::*;
+use futures::future;
+use orchestration::{
+    api::{design::Design, Orchestration},
+    common::DesignConfig,
+};
+use std::vec::Vec;
+
+fn simple_checkpoint(id: &str) {
+    info!(id = id);
+}
+
+fn location_checkpoint(id: &str, location: &str) {
+    info!(id = id, location = location);
+}
+
+async fn blocking_sleep_task() -> InvokeResult {
+    location_checkpoint("blocking_sleep_task", "begin");
+    std::thread::sleep(std::time::Duration::from_secs(1));
+    location_checkpoint("blocking_sleep_task", "end");
+    Ok(())
+}
+
+async fn basic_task_a() -> InvokeResult {
+    simple_checkpoint("basic_task_A");
+    Ok(())
+}
+
+async fn basic_task_b() -> InvokeResult {
+    simple_checkpoint("basic_task_B");
+    Ok(())
+}
+
+pub struct OneTriggerOneSyncTwoPrograms;
+
+fn trigger_sync_design() -> Result<Design, CommonErrors> {
+    let mut design = Design::new("TriggerSequence".into(), DesignConfig::default());
+
+    let blocking_sleep_task_tag = design.register_invoke_async("blocking_sleep".into(), blocking_sleep_task)?;
+    let basic_task_tag = design.register_invoke_async("basic_task_a".into(), basic_task_a)?;
+    let evt_sync = design.register_event(Tag::from_str_static("evt_sync"))?;
+
+    design.add_program("trigger_program", move |_design_instance, builder| {
+        builder.with_body(
+            SequenceBuilder::new()
+                .with_step(Invoke::from_tag(&blocking_sleep_task_tag))
+                .with_step(TriggerBuilder::from_tag(&evt_sync))
+                .build(),
+        );
+
+        Ok(())
+    });
+
+    design.add_program("sync_program", move |_design_instance, builder| {
+        builder.with_body(
+            SequenceBuilder::new()
+                .with_step(SyncBuilder::from_design("evt_sync", _design_instance))
+                .with_step(Invoke::from_tag(&basic_task_tag))
+                .build(),
+        );
+
+        Ok(())
+    });
+
+    Ok(design)
+}
+
+/// Checks trigger and sync between two programs
+impl Scenario for OneTriggerOneSyncTwoPrograms {
+    fn get_name(&self) -> &'static str {
+        "1_trigger_1_sync_2_programs"
+    }
+
+    fn run(&self, input: Option<String>) -> Result<(), String> {
+        let mut rt = Runtime::new(&input).build();
+
+        let mut orch = Orchestration::new()
+            .add_design(trigger_sync_design().expect("Failed to create design"))
+            .design_done();
+
+        let mut deployment = orch.get_deployment_mut();
+        deployment.bind_events_as_local(&["evt_sync".into()]).expect("Failed to specify event");
+
+        let mut programs = orch.create_programs().unwrap();
+        let _ = rt.block_on(async move {
+            let mut joiner = Vec::new();
+            for program in programs.programs.as_mut_slice() {
+                joiner.push(program.run_n(1));
+            }
+
+            future::join_all(joiner).await;
+            Ok(0)
+        });
+
+        Ok(())
+    }
+}
+
+pub struct OneTriggerTwoSyncsThreePrograms;
+
+fn trigger_design() -> Result<Design, CommonErrors> {
+    let mut design = Design::new("TriggerSequence".into(), DesignConfig::default());
+
+    let blocking_sleep_task_tag = design.register_invoke_async("blocking_sleep".into(), blocking_sleep_task)?;
+    let evt_sync = design.register_event(Tag::from_str_static("evt_sync"))?;
+
+    design.add_program("trigger_program", move |_design_instance, builder| {
+        builder.with_body(
+            SequenceBuilder::new()
+                .with_step(Invoke::from_tag(&blocking_sleep_task_tag))
+                .with_step(TriggerBuilder::from_tag(&evt_sync))
+                .build(),
+        );
+
+        Ok(())
+    });
+
+    Ok(design)
+}
+
+fn sync_design_a() -> Result<Design, CommonErrors> {
+    let mut design = Design::new("SyncA".into(), DesignConfig::default());
+
+    let basic_task_tag = design.register_invoke_async("basic_task_a".into(), basic_task_a)?;
+    let evt_sync = design.register_event(Tag::from_str_static("evt_sync"))?;
+
+    design.add_program("sync_program_A", move |_design_instance, builder| {
+        builder.with_body(
+            SequenceBuilder::new()
+                .with_step(SyncBuilder::from_tag(&evt_sync))
+                .with_step(Invoke::from_tag(&basic_task_tag))
+                .build(),
+        );
+
+        Ok(())
+    });
+
+    Ok(design)
+}
+
+fn sync_design_b() -> Result<Design, CommonErrors> {
+    let mut design = Design::new("SyncB".into(), DesignConfig::default());
+
+    let basic_task_tag = design.register_invoke_async("basic_task".into(), basic_task_b)?;
+    let evt_sync = design.register_event(Tag::from_str_static("evt_sync"))?;
+
+    design.add_program("sync_program_B", move |_design_instance, builder| {
+        builder.with_body(
+            SequenceBuilder::new()
+                .with_step(SyncBuilder::from_tag(&evt_sync))
+                .with_step(Invoke::from_tag(&basic_task_tag))
+                .build(),
+        );
+
+        Ok(())
+    });
+
+    Ok(design)
+}
+
+/// Checks trigger in one program and sync in other two programs
+impl Scenario for OneTriggerTwoSyncsThreePrograms {
+    fn get_name(&self) -> &'static str {
+        "1_trigger_2_syncs_3_programs"
+    }
+
+    fn run(&self, input: Option<String>) -> Result<(), String> {
+        let mut rt = Runtime::new(&input).build();
+
+        let mut orch = Orchestration::new()
+            .add_design(sync_design_a().expect("Failed to create design"))
+            .add_design(sync_design_b().expect("Failed to create design"))
+            .add_design(trigger_design().expect("Failed to create design"))
+            .design_done();
+
+        let mut deployment = orch.get_deployment_mut();
+        deployment.bind_events_as_local(&["evt_sync".into()]).expect("Failed to specify event");
+
+        let mut programs = orch.create_programs().unwrap();
+
+        let _ = rt.block_on(async move {
+            let mut joiner = Vec::new();
+            for program in programs.programs.as_mut_slice() {
+                joiner.push(program.run_n(1));
+            }
+
+            future::join_all(joiner).await;
+            Ok(0)
+        });
+
+        Ok(())
+    }
+}
+
+pub struct TriggerAndSyncInNestedBranches;
+
+fn nested_trigger_sync_design() -> Result<Design, CommonErrors> {
+    let mut design = Design::new("TriggerSequence".into(), DesignConfig::default());
+
+    let blocking_sleep_task_tag = design.register_invoke_async("blocking_sleep".into(), blocking_sleep_task)?;
+    let basic_task_tag = design.register_invoke_async("basic_task".into(), basic_task_a)?;
+    let evt_sync = design.register_event(Tag::from_str_static("evt_sync"))?;
+
+    design.add_program("trigger_sync_program", move |_design_instance, builder| {
+        builder.with_body(
+            ConcurrencyBuilder::new()
+                .with_branch(
+                    SequenceBuilder::new()
+                        .with_step(Invoke::from_tag(&blocking_sleep_task_tag))
+                        .with_step(TriggerBuilder::from_tag(&evt_sync))
+                        .build(),
+                )
+                .with_branch(
+                    SequenceBuilder::new()
+                        .with_step(SyncBuilder::from_design("evt_sync", _design_instance))
+                        .with_step(Invoke::from_tag(&basic_task_tag))
+                        .build(),
+                )
+                .build(),
+        );
+
+        Ok(())
+    });
+
+    Ok(design)
+}
+
+/// Checks trigger and sync in the separate concurrency branches in a single program
+impl Scenario for TriggerAndSyncInNestedBranches {
+    fn get_name(&self) -> &'static str {
+        "trigger_and_sync_in_nested_branches"
+    }
+
+    fn run(&self, input: Option<String>) -> Result<(), String> {
+        let mut rt = Runtime::new(&input).build();
+
+        let mut orch = Orchestration::new()
+            .add_design(nested_trigger_sync_design().expect("Failed to create design"))
+            .design_done();
+
+        let mut deployment = orch.get_deployment_mut();
+        deployment.bind_events_as_local(&["evt_sync".into()]).expect("Failed to specify event");
+
+        let mut programs = orch.create_programs().unwrap();
+
+        let _ = rt.block_on(async move {
+            let mut joiner = Vec::new();
+            for program in programs.programs.as_mut_slice() {
+                joiner.push(program.run_n(1));
+            }
+
+            future::join_all(joiner).await;
+            Ok(0)
+        });
+
+        Ok(())
+    }
+}
+
+pub struct TriggerSyncOneAfterAnother;
+
+fn trigger_sync_oaa_design() -> Result<Design, CommonErrors> {
+    let mut design = Design::new("TriggerSequence".into(), DesignConfig::default());
+
+    let basic_task_a_tag = design.register_invoke_async("basic_task_a".into(), basic_task_a)?;
+    let basic_task_b_tag = design.register_invoke_async("basic_task_b".into(), basic_task_b)?;
+    let evt_sync = design.register_event(Tag::from_str_static("evt_sync"))?;
+
+    design.add_program("trigger_sync_program", move |_design_instance, builder| {
+        builder.with_body(
+            SequenceBuilder::new()
+                .with_step(Invoke::from_tag(&basic_task_a_tag))
+                .with_step(TriggerBuilder::from_tag(&evt_sync))
+                .with_step(SyncBuilder::from_tag(&evt_sync))
+                .with_step(Invoke::from_tag(&basic_task_b_tag))
+                .build(),
+        );
+
+        Ok(())
+    });
+
+    Ok(design)
+}
+
+/// Checks trigger and sync as sequential steps in a single program
+impl Scenario for TriggerSyncOneAfterAnother {
+    fn get_name(&self) -> &'static str {
+        "trigger_sync_one_after_another"
+    }
+
+    fn run(&self, input: Option<String>) -> Result<(), String> {
+        let mut rt = Runtime::new(&input).build();
+
+        let mut orch = Orchestration::new()
+            .add_design(trigger_sync_oaa_design().expect("Failed to create design"))
+            .design_done();
+
+        let mut deployment = orch.get_deployment_mut();
+        deployment.bind_events_as_local(&["evt_sync".into()]).expect("Failed to specify event");
+
+        let mut programs = orch.create_programs().unwrap();
+
+        let _ = rt.block_on(async move {
+            let mut joiner = Vec::new();
+            for program in programs.programs.as_mut_slice() {
+                joiner.push(program.run_n(1));
+            }
+
+            future::join_all(joiner).await;
+            Ok(0)
+        });
+
+        Ok(())
+    }
+}
