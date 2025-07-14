2cf602597ee8800b9014239abc02c35707f30093
Prabakaran Chinnasamy <Prabakaran.Chinnasamy@qorix.ai>
2025-07-14 09:13:16 +0530

    example: Shutdown using signals
    Example for shutdown using Ctrl+C/SIGTERM.

---
+++ src/orchestration/examples/common/signal_handler.rs
@@ -0,0 +1,71 @@
+//
+// Copyright (c) 2025 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache License Version 2.0 which is available at
+// <https://www.apache.org/licenses/LICENSE-2.0>
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#![allow(dead_code)]
+
+use ::core::{
+    sync::atomic::{AtomicI32, Ordering},
+    time::Duration,
+};
+use libc::{sigaction, sighandler_t, SA_RESTART, SIGINT, SIGTERM};
+
+pub struct SignalHandler {
+    signal: AtomicI32,
+}
+
+/// This struct provides a way to handle signals SIGINT and SIGTERM.
+impl SignalHandler {
+    /// Returns a reference to the singleton instance of `SignalHandler`.
+    pub fn get_instance() -> &'static Self {
+        &SIGNAL_HANDLER
+    }
+
+    /// Registers signal handlers for SIGINT and SIGTERM.
+    pub unsafe fn register_signal_handlers(&self) {
+        let mut action: sigaction = std::mem::zeroed();
+        action.sa_sigaction = handler as sighandler_t;
+        action.sa_flags = SA_RESTART;
+
+        sigaction(SIGINT, &action, std::ptr::null_mut());
+        sigaction(SIGTERM, &action, std::ptr::null_mut());
+    }
+
+    /// Returns the current signal value.
+    pub fn is_signal_received(&self) -> i32 {
+        self.signal.load(Ordering::SeqCst)
+    }
+
+    /// Waits until a signal is received.
+    pub fn wait_until_signal_received(&self) -> i32 {
+        let mut received_signal;
+
+        loop {
+            // Check if a signal has been received
+            received_signal = self.signal.load(Ordering::SeqCst);
+            if received_signal != 0 {
+                break;
+            }
+
+            // Sleep for a short duration to avoid busy waiting
+            std::thread::sleep(Duration::from_millis(20));
+        }
+
+        received_signal
+    }
+}
+
+static SIGNAL_HANDLER: SignalHandler = SignalHandler { signal: AtomicI32::new(0) };
+
+extern "C" fn handler(sig: i32) {
+    SIGNAL_HANDLER.signal.store(sig, Ordering::SeqCst);
+}
---
+++ src/orchestration/examples/shutdown_using_signals.rs
@@ -0,0 +1,99 @@
+//
+// Copyright (c) 2025 Contributors to the Eclipse Foundation
+//
+// See the NOTICE file(s) distributed with this work for additional
+// information regarding copyright ownership.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Apache License Version 2.0 which is available at
+// <https://www.apache.org/licenses/LICENSE-2.0>
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+use ::core::future;
+use async_runtime::{runtime::async_runtime::AsyncRuntimeBuilder, scheduler::execution_engine::*};
+use foundation::prelude::*;
+use logging_tracing::{TraceScope, TracingLibraryBuilder};
+use orchestration::{
+    api::{design::Design, Orchestration},
+    common::DesignConfig,
+    prelude::Invoke,
+};
+
+#[path = "common/signal_handler.rs"]
+mod signal_handler;
+use signal_handler::SignalHandler;
+
+fn example_component_design() -> Result<Design, CommonErrors> {
+    let mut design = Design::new("ExampleDesign".into(), DesignConfig::default());
+    let t1 = design.register_invoke_async("PendingIndefinitely".into(), async || future::pending().await)?;
+    design.register_shutdown_event("ShutdownEvent".into())?;
+
+    design.add_program("ExampleDesignProgram", move |_design, builder| {
+        builder.with_run_action(Invoke::from_tag(&t1)).with_shutdown_event("ShutdownEvent".into());
+
+        Ok(())
+    });
+
+    Ok(design)
+}
+
+fn main() {
+    // Setup any logging framework you want to use.
+    let mut logger = TracingLibraryBuilder::new()
+        .global_log_level(Level::INFO)
+        .enable_tracing(TraceScope::AppScope)
+        .enable_logging(true)
+        .build();
+
+    logger.init_log_trace();
+
+    // Create runtime
+    let (builder, _engine_id) = AsyncRuntimeBuilder::new().with_engine(ExecutionEngineBuilder::new().task_queue_size(256).workers(2));
+    let mut runtime = builder.build().unwrap();
+
+    // Build Orchestration
+    let mut orch = Orchestration::new()
+        .add_design(example_component_design().expect("Failed to create design"))
+        .design_done();
+
+    // Deployment part - specify event details
+    let mut deployment = orch.get_deployment_mut();
+    deployment
+        .bind_shutdown_event_as_local("ShutdownEvent".into())
+        .expect("Failed to bind shutdown event");
+
+    // Get shutdown notifier to shutdown the program when shutdown is requested
+    let mut shutdown_notifier = deployment
+        .get_shutdown_notifier("ShutdownEvent".into())
+        .expect("Failed to get shutdown notifier");
+
+    // Create program
+    let mut programs = orch.create_programs().unwrap();
+    let mut program = programs.programs.pop().unwrap();
+
+    // Register signal handlers for SIGINT and SIGTERM
+    unsafe { SignalHandler::get_instance().register_signal_handlers() };
+
+    // Put programs into runtime and run them
+    let _ = runtime.spawn(async move {
+        let _ = program.run().await;
+        info!("Program terminated.");
+        Ok(0)
+    });
+    info!("Runtime spawned. Running the program...");
+    info!("Press Ctrl+C or send SIGTERM to terminate the program.");
+
+    // Wait for shutdown signal
+    let received_signal = SignalHandler::get_instance().wait_until_signal_received();
+    info!("Received signal: {}", received_signal);
+
+    // Shutdown the program
+    let _ = shutdown_notifier.shutdown();
+
+    // Wait for all engines to finish
+    runtime.wait_for_all_engines();
+
+    info!("Exit.");
+}
