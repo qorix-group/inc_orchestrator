99154432c22372ea01b9310f535a1c3e840f5f8e
Piotr Korkus <piotr.korkus.ext@qorix.ai>
2025-10-15 11:03:58 +0200

    testing: orchestration bind to dedicated worker
    dedicated workers fully occupied
    dedicated workers reused to regular tasks
    dedicated workers repeated tag assignment
    dedicated workers non existent tag assignment
    dedicated workers non existent worker assignment

---
+++ component_integration_tests/python_test_cases/tests/orchestrator/test_bind_to_worker.py
@@ -0,0 +1,127 @@
+from typing import Any
+
+import pytest
+from testing_utils import ScenarioResult
+from testing_utils.log_container import LogContainer
+
+from component_integration_tests.python_test_cases.tests.cit_scenario import (
+    CitScenario,
+)
+from component_integration_tests.python_test_cases.tests.result_code import (
+    ResultCode,
+)
+
+
+class CommonDedicatedWorkerConfig(CitScenario):
+    @pytest.fixture(scope="class")
+    def num_dedicated(self) -> int:
+        return 3
+
+    @pytest.fixture(scope="class")
+    def dedicated_workers(self, num_dedicated: int) -> list[dict[str, Any]]:
+        result = []
+        for i in range(num_dedicated):
+            result.append({"id": f"dedicated_worker_{i}"})
+        return result
+
+    @pytest.fixture(scope="class")
+    def test_config(self, dedicated_workers: list[dict[str, Any]]) -> dict[str, Any]:
+        return {
+            "runtime": {
+                "task_queue_size": 256,
+                "workers": 1,
+                "dedicated_workers": dedicated_workers,
+            }
+        }
+
+
+class TestDedicatedWorkerBindTags(CommonDedicatedWorkerConfig):
+    @pytest.fixture(scope="class")
+    def scenario_name(self) -> str:
+        return "orchestration.dedicated_worker.bind_tags"
+
+    def test_dedicated_assignment(self, logs_info_level: LogContainer):
+        # All async functions are assigned to the same dedicated worker 0
+        logs_dedicated = logs_info_level.get_logs(field="message", pattern=r"\'async\d\'.*")
+        dedicated_threads = {log.thread_id for log in logs_dedicated}
+        assert len(dedicated_threads) == 1, "Expected execution only on 1 dedicated worker"
+
+        # Sync1 task is assigned to dedicated worker 1
+        logs_sync1 = logs_info_level.get_logs(field="message", pattern=r"\'sync1\'.*")
+        sync1_threads = {log.thread_id for log in logs_sync1}
+        assert len(sync1_threads) == 1, "Expected execution only on 1 dedicated"
+
+        # Sync2 task is assigned to dedicated worker 2
+        logs_sync2 = logs_info_level.get_logs(field="message", pattern=r"\'sync2\'.*")
+        sync2_threads = {log.thread_id for log in logs_sync2}
+        assert len(sync2_threads) == 1, "Expected execution only on 1 dedicated"
+
+
+class TestDedicatedWorkerReuse(CommonDedicatedWorkerConfig):
+    @pytest.fixture(scope="class")
+    def num_dedicated(self) -> int:
+        return 1
+
+    @pytest.fixture(scope="class")
+    def scenario_name(self) -> str:
+        return "orchestration.dedicated_worker.dedicated_works_on_regular"
+
+    def test_dedicated_assignment(self, logs_info_level: LogContainer):
+        # Find out which thread was assigned to the 'sync1' task - it should be a dedicated worker
+        dedicated_thread_id = logs_info_level.get_logs(field="message", pattern=r"\'sync1\'")[0].thread_id
+
+        # Find all other tasks in design and extract their thread ids
+        other_logs = logs_info_level.get_logs(field="message", pattern=r"^(?!.*'sync1').*$")
+        other_thread_ids = {log.thread_id for log in other_logs}
+
+        # Check that the dedicated worker thread was reused for other tasks
+        assert dedicated_thread_id in other_thread_ids, "Dedicated worker should be reused for other tasks"
+
+
+class TestDedicatedWorkerRepeatedAssignment(CommonDedicatedWorkerConfig):
+    @pytest.fixture(scope="class")
+    def scenario_name(self) -> str:
+        return "orchestration.dedicated_worker.repeat_tag_assignment"
+
+    def capture_stderr(self):
+        return True
+
+    def expect_command_failure(self):
+        return True
+
+    def test_expected_error(self, results: ScenarioResult):
+        assert results.return_code == ResultCode.PANIC
+        assert "Failed to bind invoke action to worker: AlreadyDone" in results.stderr
+
+
+class TestNonExistentTag(CommonDedicatedWorkerConfig):
+    @pytest.fixture(scope="class")
+    def scenario_name(self) -> str:
+        return "orchestration.dedicated_worker.assign_non_existent_tag"
+
+    def capture_stderr(self):
+        return True
+
+    def expect_command_failure(self):
+        return True
+
+    def test_expected_error(self, results: ScenarioResult):
+        assert results.return_code == ResultCode.PANIC
+        assert "Failed to bind invoke action to worker: NotFound" in results.stderr
+
+
+class TestNonExistentDedicatedWorker(CommonDedicatedWorkerConfig):
+    @pytest.fixture(scope="class")
+    def scenario_name(self) -> str:
+        return "orchestration.dedicated_worker.assign_to_non_existent_dedicated_worker"
+
+    def capture_stderr(self):
+        return True
+
+    def expect_command_failure(self):
+        return True
+
+    @pytest.mark.xfail(reason="Currently fails with SIGABRT instead of PANIC")
+    def test_expected_error(self, results: ScenarioResult):
+        assert results.return_code == ResultCode.PANIC
+        assert "Tried to spawn on not registered dedicated worker" in results.stderr
---
+++ component_integration_tests/rust_test_scenarios/src/tests/orchestration/orchestration_dedicated_worker.rs
@@ -0,0 +1,194 @@
+use super::*;
+use crate::internals::runtime_helper::Runtime;
+use foundation::prelude::CommonErrors;
+use orchestration::api::design::Design;
+use orchestration::api::Orchestration;
+use orchestration::common::DesignConfig;
+use test_scenarios_rust::scenario::Scenario;
+
+fn dedicated_design() -> Result<Design, CommonErrors> {
+    let mut design = Design::new("SingleSequence".into(), DesignConfig::default());
+
+    let sync_tag_1 = design.register_invoke_fn("sync1".into(), generic_test_func!("sync1"))?;
+    let sync_tag_2 = design.register_invoke_fn("sync2".into(), generic_test_func!("sync2"))?;
+
+    let async_tag_1 = design.register_invoke_async("async1".into(), generic_async_test_func!("async1"))?;
+    let async_tag_2 = design.register_invoke_async("async2".into(), generic_async_test_func!("async2"))?;
+
+    // Create a program with actions
+    design.add_program(file!(), move |_design_instance, builder| {
+        builder.with_run_action(
+            ConcurrencyBuilder::new()
+                .with_branch(Invoke::from_tag(&async_tag_1, _design_instance.config()))
+                .with_branch(Invoke::from_tag(&async_tag_2, _design_instance.config()))
+                .with_branch(Invoke::from_tag(&sync_tag_1, _design_instance.config()))
+                .with_branch(Invoke::from_tag(&sync_tag_2, _design_instance.config()))
+                .build(_design_instance),
+        );
+
+        Ok(())
+    });
+
+    Ok(design)
+}
+
+struct DedicatedWorkerBindTags;
+
+impl Scenario for DedicatedWorkerBindTags {
+    fn name(&self) -> &str {
+        "bind_tags"
+    }
+
+    fn run(&self, input: &str) -> Result<(), String> {
+        let builder = Runtime::from_json(input)?;
+        let mut rt = builder.build();
+
+        let mut orch = Orchestration::new()
+            .add_design(dedicated_design().expect("Failed to create design"))
+            .design_done();
+        let mut deployment = orch.get_deployment_mut();
+
+        deployment
+            .bind_invoke_to_worker("async1".into(), "dedicated_worker_0".into())
+            .expect("Failed to bind invoke action to worker");
+        deployment
+            .bind_invoke_to_worker("async2".into(), "dedicated_worker_0".into())
+            .expect("Failed to bind invoke action to worker");
+        deployment
+            .bind_invoke_to_worker("sync1".into(), "dedicated_worker_1".into())
+            .expect("Failed to bind invoke action to worker");
+        deployment
+            .bind_invoke_to_worker("sync2".into(), "dedicated_worker_2".into())
+            .expect("Failed to bind invoke action to worker");
+
+        let mut program_manager = orch.into_program_manager().expect("Failed to create programs");
+        let mut programs = program_manager.get_programs();
+
+        rt.block_on(async move {
+            let mut program = programs.pop().expect("Failed to pop program");
+            let _ = program.run_n(1).await;
+        });
+        Ok(())
+    }
+}
+
+struct DedicatedWorkerRepeatedAssignment;
+impl Scenario for DedicatedWorkerRepeatedAssignment {
+    fn name(&self) -> &str {
+        "repeat_tag_assignment"
+    }
+
+    fn run(&self, input: &str) -> Result<(), String> {
+        let builder = Runtime::from_json(input)?;
+        let mut rt = builder.build();
+
+        let mut orch = Orchestration::new()
+            .add_design(dedicated_design().expect("Failed to create design"))
+            .design_done();
+        let mut deployment = orch.get_deployment_mut();
+
+        deployment
+            .bind_invoke_to_worker("async1".into(), "dedicated_worker_0".into())
+            .expect("Failed to bind invoke action to worker");
+        deployment
+            .bind_invoke_to_worker("async1".into(), "dedicated_worker_1".into())
+            .expect("Failed to bind invoke action to worker");
+
+        let mut program_manager = orch.into_program_manager().expect("Failed to create programs");
+        let mut programs = program_manager.get_programs();
+
+        rt.block_on(async move {
+            let mut program = programs.pop().expect("Failed to pop program");
+            let _ = program.run_n(1).await;
+        });
+        Ok(())
+    }
+}
+
+struct DedicatedWorkerNonExistentTag;
+impl Scenario for DedicatedWorkerNonExistentTag {
+    fn name(&self) -> &str {
+        "assign_non_existent_tag"
+    }
+
+    fn run(&self, input: &str) -> Result<(), String> {
+        let _builder = Runtime::from_json(input)?;
+
+        let mut orch = Orchestration::new()
+            .add_design(dedicated_design().expect("Failed to create design"))
+            .design_done();
+        let mut deployment = orch.get_deployment_mut();
+
+        deployment
+            .bind_invoke_to_worker("non_existent_tag".into(), "dedicated_worker_0".into())
+            .expect("Failed to bind invoke action to worker");
+
+        Ok(())
+    }
+}
+
+struct DedicatedWorkerNonExistent;
+impl Scenario for DedicatedWorkerNonExistent {
+    fn name(&self) -> &str {
+        "assign_to_non_existent_dedicated_worker"
+    }
+
+    fn run(&self, input: &str) -> Result<(), String> {
+        let _builder = Runtime::from_json(input)?;
+
+        let mut orch = Orchestration::new()
+            .add_design(dedicated_design().expect("Failed to create design"))
+            .design_done();
+        let mut deployment = orch.get_deployment_mut();
+
+        deployment
+            .bind_invoke_to_worker("async1".into(), "non_existent_dedicated_worker".into())
+            .expect("Failed to bind invoke action to worker");
+
+        Ok(())
+    }
+}
+
+struct DedicatedWorkerReuse;
+impl Scenario for DedicatedWorkerReuse {
+    fn name(&self) -> &str {
+        "dedicated_works_on_regular"
+    }
+
+    fn run(&self, input: &str) -> Result<(), String> {
+        let builder = Runtime::from_json(input)?;
+        let mut rt = builder.build();
+
+        let mut orch = Orchestration::new()
+            .add_design(dedicated_design().expect("Failed to create design"))
+            .design_done();
+        let mut deployment = orch.get_deployment_mut();
+
+        deployment
+            .bind_invoke_to_worker("sync1".into(), "dedicated_worker_0".into())
+            .expect("Failed to bind invoke action to worker");
+
+        let mut program_manager = orch.into_program_manager().expect("Failed to create programs");
+        let mut programs = program_manager.get_programs();
+
+        rt.block_on(async move {
+            let mut program = programs.pop().expect("Failed to pop program");
+            let _ = program.run_n(1).await;
+        });
+        Ok(())
+    }
+}
+
+pub fn dedicated_worker_scenario_group() -> Box<dyn ScenarioGroup> {
+    Box::new(ScenarioGroupImpl::new(
+        "dedicated_worker",
+        vec![
+            Box::new(DedicatedWorkerBindTags),
+            Box::new(DedicatedWorkerReuse),
+            Box::new(DedicatedWorkerRepeatedAssignment),
+            Box::new(DedicatedWorkerNonExistentTag),
+            Box::new(DedicatedWorkerNonExistent),
+        ],
+        vec![],
+    ))
+}
