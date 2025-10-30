9e3d10187f45367b1a69a8a5737dfad3b19a9d10
Pawel Rutka <pawel.rutka.ext@qorix.ai>
2025-10-30 15:07:39 +0100

    qnx8: make virtualization setup

--- scripts/run_on_qnx8.sh
+++ scripts/run_on_qnx8.sh
@@ -3,26 +3,17 @@
 # Exit immediately if any command fails
 set -e
 
-echo "Does not work yet"
-if [ -f "virtualization/_BUILD" ]; then
-  echo "Error: _BUILD file found in src/virtualization/. Please rename it to BUILD. If You do so, //.... target will stop working until this is not reverted. Sorry for the inconvenience."
-  exit 1
-fi
+echo "----------- CURRENTLY VIRTUALIZATION IS DISABLED FOR QNX8. TO ENABLE SET `disable = False` flag in virtualization/BUILD -----------"
 
+# Check if SCORE_QNX_USER and SCORE_QNX_PASSWORD are set
+if [[ -z "${SCORE_QNX_USER}" || -z "${SCORE_QNX_PASSWORD}" ]]; then
+  # If either is missing, check for .netrc
   if [[ ! -f "$HOME/.netrc" ]]; then
     echo "Error: Either SCORE_QNX_USER/SCORE_QNX_PASSWORD must be set, or $HOME/.netrc must exist."
-# # Check if SCORE_QNX_USER is set
-# if [[ -z "${SCORE_QNX_USER}" ]]; then
-#   echo "Error: SCORE_QNX_USER is not set. This shall be myQNX portal username."
-#   exit 1
-# fi
+    exit 1
+  fi
+fi
 
-# # Check if SCORE_QNX_PASSWORD is set
-# if [[ -z "${SCORE_QNX_PASSWORD}" ]]; then
-#   echo "Error: SCORE_QNX_PASSWORD is not set. This shall be myQNX portal password."
-#   exit 1
-# fi
+echo "Note: If it fails with 'Error downloading [https://www.qnx.com/download/download/79858/installation.tgz' access this link from webbrowser and accept the license agreement."
 
-# echo "Note: If it fails with 'Error downloading [https://www.qnx.com/download/download/79858/installation.tgz' access this link from webbrowser and accept the license agreement."
-
-# bazel run --sandbox_debug --verbose_failures --platforms=@score_toolchains_rust//platforms:aarch64-unknown-qnx8_0 --credential_helper=*.qnx.com=%workspace%/scripts/internal/qnx_creds.py //virtualization:run_qemu
+bazel run  --config=build_qnx8 --credential_helper=*.qnx.com=%workspace%/scripts/internal/qnx_creds.py //virtualization:virtualization_run_qemu
---
+++ virtualization/custom_init.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo "Custom init can be added here"
