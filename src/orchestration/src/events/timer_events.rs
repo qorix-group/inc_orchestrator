e03015bb043eedcc62e0ca5df421c6c189729719
Pawel Rutka <pawel.rutka.ext@qorix.ai>
2025-11-19 11:31:13 +0100

    Temp fixes

--- src/orchestration/src/events/timer_events.rs
+++ src/orchestration/src/events/timer_events.rs
@@ -12,19 +12,20 @@
 //
 
 use core::time::Duration;
+use std::sync::{atomic::AtomicU64, Arc};
 
 use async_runtime::{
     futures::sleep,
     time::clock::{Clock, Instant},
 };
-use foundation::prelude::warn;
+use foundation::prelude::*;
 
 use crate::events::event_traits::ListenerTrait;
 
 pub(crate) struct TimerEvent {
     start_time: Option<Instant>,
     cycle_duration: core::time::Duration,
-    tick: i128,
+    tick: Arc<AtomicU64>,
 }
 
 impl TimerEvent {
@@ -32,7 +33,7 @@ impl TimerEvent {
         TimerEvent {
             start_time: None,
             cycle_duration,
-            tick: -1,
+            tick: Arc::new(AtomicU64::new(0)),
         }
     }
 }
@@ -40,37 +41,57 @@ impl TimerEvent {
 impl ListenerTrait for TimerEvent {
     fn next(&mut self) -> impl core::future::Future<Output = crate::prelude::ActionResult> + Send + 'static {
         // TODO: Fix the issue that next() i called before iteration to create future
-        self.tick += 1;
 
         let is_first_time = self.start_time.is_none();
         if is_first_time {
-            self.start_time = Some(Clock::now());
+            let now: Instant = Clock::now();
+            let now_ns = now.elapsed().as_nanos();
+            let rest = now_ns % self.cycle_duration.as_nanos();
+            let missing = now_ns - rest;
+            self.start_time = Some(now.checked_add(Duration::from_nanos(missing as u64)).unwrap());
+            info!("Starting TimerEvent at {:?}, first tick in {} ns", self.start_time.unwrap(), missing);
+        } else {
+            self.tick.fetch_add(1, std::sync::atomic::Ordering::AcqRel);
         }
 
         let start_time = self.start_time.unwrap();
         let cycle = self.cycle_duration;
-        let tick = self.tick;
+        let tick = self.tick.clone();
 
-        // TODO: fix when mio is providing timer events, currently we use sleep
+        // TODO: fix when mio is providing timer events/ or timewheel provides interval, currently we use sleep
         async move {
-            let elapsed = Clock::now().saturating_duration_since(start_time).as_millis();
-            let elapsed_in_full_cycles = cycle.as_millis() * tick as u128;
+            let now = Clock::now();
+            let elapsed = now.saturating_duration_since(start_time).as_millis();
+            let elapsed_ticks = (elapsed / cycle.as_millis()) as u64;
+            let expected_ticks = tick.load(std::sync::atomic::Ordering::Relaxed);
 
-            match elapsed.cmp(&elapsed_in_full_cycles) {
-                core::cmp::Ordering::Less => {
-                    let remaining = elapsed_in_full_cycles - elapsed;
-                    let remaining_duration = Duration::from_millis(remaining as u64);
-                    sleep::sleep(remaining_duration).await;
-                }
-                core::cmp::Ordering::Equal => {}
-                core::cmp::Ordering::Greater => {
-                    warn!(
-                        "TimerEvent: cycle duration exceeded, expected cycle: {}, elapsed: {} in iteration {}",
-                        cycle.as_millis(),
-                        elapsed,
-                        tick
-                    );
-                }
+            info!(
+                "TimerEvent Tick: now={:?} elapsed={} elapsed_ticks={}, expected_ticks={}",
+                now, elapsed, elapsed_ticks, expected_ticks
+            );
+
+            if elapsed_ticks > expected_ticks {
+                tick.store(elapsed_ticks, std::sync::atomic::Ordering::Release);
+                warn!(
+                    "Expected that we are at timer event tick {} but we are on {}. Missed {} ticks!",
+                    expected_ticks,
+                    elapsed_ticks,
+                    elapsed_ticks - expected_ticks
+                );
+            } else {
+                let elapsed_in_full_cycles = cycle.as_millis() * (expected_ticks) as u128;
+
+                match elapsed.cmp(&elapsed_in_full_cycles) {
+                    core::cmp::Ordering::Less => {
+                        let remaining = elapsed_in_full_cycles - elapsed;
+                        let remaining_duration = Duration::from_millis(remaining as u64);
+
+                        info!("Will sleep for {:?}", remaining_duration);
+                        sleep::sleep(remaining_duration).await;
+                    }
+                    core::cmp::Ordering::Equal => {}
+                    core::cmp::Ordering::Greater => {}
+                };
             }
 
             Ok(())
