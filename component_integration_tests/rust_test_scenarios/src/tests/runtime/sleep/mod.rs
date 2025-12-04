718121cbf012fed438f2b2e1bfa868076f69fe86
Pawel Rutka <pawel.rutka.ext@qorix.ai>
2025-12-04 15:33:31 +0100

    migration: remove kyron

--- component_integration_tests/rust_test_scenarios/src/tests/runtime/sleep/mod.rs
+++
@@ -1,7 +0,0 @@
-mod sleep_basic;
-
-use test_scenarios_rust::scenario::{ScenarioGroup, ScenarioGroupImpl};
-
-pub fn sleep_scenario_group() -> Box<dyn ScenarioGroup> {
-    Box::new(ScenarioGroupImpl::new("sleep", vec![Box::new(sleep_basic::SleepBasic)], vec![]))
-}
